99a6c99b3a8c2e1cd2bb3882d87f364f51c2e991
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-24 10:06:41 +0530

    a is cancell
    a ko hataya h abhi k liye

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var h1 = document.getElementById("h1");
 var bt1 = document.querySelector("#bt1");
 
-bt1.addEventListener("click", function a() {
+bt1.addEventListener("click", function() {
     
     h1.innerHTML = "welcome to coding home -please strat coding......"
     //  h1.style.textcolore = "red"
