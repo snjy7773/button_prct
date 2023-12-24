94a734afa9501caf0982525b700eed49a6bd9358
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-24 09:26:16 +0530

    now its working....
    chalu h

---
+++ main.js
@@ -0,0 +1,10 @@
+var h1 = document.getElementById("h1");
+var bt1 = document.querySelector("#bt1");
+
+bt1.addEventListener("click", function a() {
+    
+    h1.innerHTML = "welcome to coding home -please strat coding......"
+    //  h1.style.textcolore = "red"
+    
+});
+ 
