9c8a12e9f59281f94ddc4f78a915a1285fc49c19
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-26 19:00:09 +0530

    [2] swap to click done

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
+// [1]=> click a button text color will  change.
 var h1 = document.getElementById("h1");
 var bt1 = document.querySelector("#bt1");
 
-// bt1.addEventListener("click", function() {
-    
-//     h1.innerHTML = "welcome to coding home -please strat coding......"
-//     h1.style.color = "red"
-    
-// });
  
 bt1.addEventListener("click", function() {
     
@@ -18,15 +13,17 @@ bt1.addEventListener("click", function() {
 // [2]=> click a button and img position change like swap....
     var img1 = document.querySelector("#img1");
     var img2 = document.querySelector("#img2");
+   var bt2 = document.querySelector("#bt2");
     // document.querySelector();
-document.querySelector("#bt2").addEventListener("click", function(){
+bt2.addEventListener("click", function(){
     // alert("chal raha h");
-    img2.src =  img1.src; 
-    img1.src = img2.src;   
     var a= img1.src ;
     var b= img2.src ;
-   console.log(a,b);
-})
-   
+    img1.src = b;   
+    img2.src = a; 
+
+});
+
+  // 
 
 
