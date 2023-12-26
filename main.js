581fe36ff8d43212179fcbda281c5ea9f641b6eb
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-26 16:11:44 +0530

    next

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 var h1 = document.getElementById("h1");
 var bt1 = document.querySelector("#bt1");
 
+// bt1.addEventListener("click", function() {
+    
+//     h1.innerHTML = "welcome to coding home -please strat coding......"
+//     h1.style.color = "red"
+    
+// });
+ 
 bt1.addEventListener("click", function() {
     
-    h1.innerHTML = "welcome to coding home -please strat coding......"
-    //  h1.style.textcolore = "red"
+    h1.textContent = "PRACTICE MEJOR BUTTON EVENTS....";
+    h1.style.color = "pink"
+    
     
 });
  
