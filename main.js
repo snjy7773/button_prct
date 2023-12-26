a5b0f8cc43931e99db12b35d12f5fc3848e298af
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-26 19:51:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+
+
+
 // [1]=> click a button text color will  change.
 var h1 = document.getElementById("h1");
 var bt1 = document.querySelector("#bt1");
 
+bt1.addEventListener("click", function() {
+h1.textContent = "PRACTICE MEJOR BUTTON EVENTS....";
+h1.style.color = "blue"
+});
 
  
-bt1.addEventListener("click", function() {
-    
-    h1.textContent = "PRACTICE MEJOR BUTTON EVENTS....";
-    h1.style.color = "blue"
-});
+
  
+
+
+
 //[2]=> click button to change text 
 var h2 = document.querySelector("#h2");
 var bt3 = document.querySelector("#bt3");
@@ -19,12 +25,15 @@ bt3.addEventListener("click", function(){
     // alert("chalen 6")
 })
 
+
+
+
 // [3]=> click a button and img position change like swap....
-    var img1 = document.querySelector("#img1");
+var img1 = document.querySelector("#img1");
     var img2 = document.querySelector("#img2");
    var bt2 = document.querySelector("#bt2");
-    // document.querySelector();
-bt2.addEventListener("click", function(){
+
+   bt2.addEventListener("click", function(){
     // alert("chal raha h");
     var a= img1.src ;
     var b= img2.src ;
@@ -33,6 +42,6 @@ bt2.addEventListener("click", function(){
 
 });
 
-  // 
+   
 
 
