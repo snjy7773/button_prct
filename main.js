b5cab0184e7479e86751b7ad2c764a276c036ec1
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-26 18:27:02 +0530

    add some img

--- main.js
+++ main.js
@@ -12,7 +12,21 @@ bt1.addEventListener("click", function() {
     
     h1.textContent = "PRACTICE MEJOR BUTTON EVENTS....";
     h1.style.color = "pink"
-    
-    
 });
  
+
+// [2]=> click a button and img position change like swap....
+    var img1 = document.querySelector("#img1");
+    var img2 = document.querySelector("#img2");
+    // document.querySelector();
+document.querySelector("#bt2").addEventListener("click", function(){
+    // alert("chal raha h");
+    img2.src =  img1.src; 
+    img1.src = img2.src;   
+    var a= img1.src ;
+    var b= img2.src ;
+   console.log(a,b);
+})
+   
+
+
